995967d997b548986d572ddc6bf9f3aa7276cc2e
404UsernameNotFound404 <2001henrymorris@gmail.com>
2020-01-11 17:55:47 -0500

    stuff

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }), cors());
 var uri = "mongodb://Chris:meme@studentstuff-shard-00-00-qyuqj.gcp.mongodb.net:27017,studentstuff-shard-00-01-qyuqj.gcp.mongodb.net:27017,studentstuff-shard-00-02-qyuqj.gcp.mongodb.net:27017/test?ssl=true&replicaSet=studentstuff-shard-0&authSource=admin&retryWrites=true&w=majority";
 MongoClient.connect(uri, function(err, client) {
   const collection = client.db("test").collection("devices");
-  require("./src/routes")(app, database);
+  require("./src/routes")(app, client);
   app.listen(8000, () => {
     console.log("Listening on port " + 8000);
   });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const assert = require("assert");
 let OBjectID = require("mongodb").ObjectID;
-const bcypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { JWT_KEY } = require("../../envorment.json");
+// const { JWT_KEY } = require("../../envorment.json");
 
 module.exports = function(app, db) { 
     app.get('/test', (req, res) => {
+        console.log(req.body)
         console.log("asd")
+        res.send({res: "back"})
     })
 }
